f3d717f3d750515a3d32321b5420453a17b34c1e
tomo <o_tomo_1990@yahoo.co.jp>
2023-02-05 20:56:55 +0900

    トップページに作成済みのメモのリストを表示する

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
+  @memos = Dir.glob('views/*').map { |f| File.basename(f).chomp('.erb') }.delete_if{|name| name == 'index' || name == 'layout'}
   erb :index
 end
 
