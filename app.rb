16e53fd6c005d87114e68db57a5245772b5f878b
tomo <o_tomo_1990@yahoo.co.jp>
2023-02-05 21:06:49 +0900

    memoデータをmarkdownファイルに変更

--- app.rb
+++ app.rb
@@ -2,10 +2,10 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  @memos = Dir.glob('views/*').map { |f| File.basename(f).chomp('.erb') }.delete_if{|name| name == 'index' || name == 'layout'}
+  @memos = Dir.glob('views/*.markdown').map { |f| File.basename(f).chomp('.markdown') }
   erb :index
 end
 
 get '/memos/:title' do
-  erb params[:title].to_sym
+  erb :memo_template, :locals => { :md => markdown(params[:title].to_sym) }
 end
