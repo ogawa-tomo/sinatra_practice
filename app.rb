33cb0636d512640c2628f6cc23e3deabb3e0a1cb
tomo <o_tomo_1990@yahoo.co.jp>
2023-02-19 14:25:00 +0900

    SQLインジェクション対策としてSQLをプレースホルダで実装

--- app.rb
+++ app.rb
@@ -25,29 +25,29 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-  @memo = CONN.exec("select * from memo where id = \'#{params[:id]}\'")[0]
+  @memo = CONN.exec_params("select * from memo where id = $1", [params[:id]])[0]
   erb :memo_template
 end
 
 post '/memos' do
   title = params[:title]
   body = params[:body]
-  result = CONN.exec("insert into memo (title, body, created_at, updated_at) values (\'#{title}\', \'#{body}\', \'#{Time.now}\', \'#{Time.now}\') returning id;")
+  result = CONN.exec_params("insert into memo (title, body, created_at, updated_at) values ($1, $2, $3, $4) returning id;", [title, body, Time.now, Time.now])
   redirect to("/memos/#{result[0]['id']}")
 end
 
 get '/memos/:id/edit' do
-  @memo = CONN.exec("select * from memo where id = \'#{params[:id]}\'")[0]
+  @memo = CONN.exec_params("select * from memo where id = $1", [params[:id]])[0]
   erb :edit
 end
 
 patch '/memos/:id' do
-  CONN.exec("update memo set title = \'#{params[:title]}\', body = \'#{params[:body]}\', updated_at = \'#{Time.now}\' where id = #{params[:id]};")
+  CONN.exec_params("update memo set title = $1, body = $2, updated_at = $3 where id = $4;", [params[:title], params[:body], Time.now, params[:id]])
   redirect to("/memos/#{params[:id]}")
 end
 
 delete '/memos/:id' do
-  CONN.exec("delete from memo where id = #{params[:id]};")
+  CONN.exec_params("delete from memo where id = $1;", [params[:id]])
   redirect to('/')
 end
 
