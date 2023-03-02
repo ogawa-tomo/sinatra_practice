d44008202f43cfa5d3979afa156567694b481ee4
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-02 21:02:27 +0900

    SQLのキーワードを大文字に修正

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ CONN = PG::Connection.new(
 )
 
 get '/' do
-  @memos = CONN.exec('select * from memo order by updated_at desc;')
+  @memos = CONN.exec('SELECT * FROM memo ORDER BY updated_at DESC;')
   erb :index
 end
 
@@ -25,29 +25,29 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-  @memo = CONN.exec_params('select * from memo where id = $1', [params[:id]])[0]
+  @memo = CONN.exec_params('SELECT * FROM memo WHERE id = $1', [params[:id]])[0]
   erb :memo_template
 end
 
 post '/memos' do
   title = params[:title]
   body = params[:body]
-  result = CONN.exec_params('insert into memo (title, body, created_at, updated_at) values ($1, $2, $3, $4) returning id;', [title, body, Time.now, Time.now])
+  result = CONN.exec_params('INSERT INTO memo (title, body, created_at, updated_at) VALUES ($1, $2, $3, $4) RETURNING id;', [title, body, Time.now, Time.now])
   redirect to("/memos/#{result[0]['id']}")
 end
 
 get '/memos/:id/edit' do
-  @memo = CONN.exec_params('select * from memo where id = $1', [params[:id]])[0]
+  @memo = CONN.exec_params('SELECT * FROM memo WHERE id = $1', [params[:id]])[0]
   erb :edit
 end
 
 patch '/memos/:id' do
-  CONN.exec_params('update memo set title = $1, body = $2, updated_at = $3 where id = $4;', [params[:title], params[:body], Time.now, params[:id]])
+  CONN.exec_params('UPDATE memo SET title = $1, body = $2, updated_at = $3 WHERE id = $4;', [params[:title], params[:body], Time.now, params[:id]])
   redirect to("/memos/#{params[:id]}")
 end
 
 delete '/memos/:id' do
-  CONN.exec_params('delete from memo where id = $1;', [params[:id]])
+  CONN.exec_params('DELETE FROM memo WHERE id = $1;', [params[:id]])
   redirect to('/')
 end
 
